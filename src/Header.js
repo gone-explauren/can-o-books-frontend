1395eb596b5eac3eca77a38e47af79620d66d74f
Laurel Perkins <lmperkins20@gmail.com>
2023-03-15 16:53:48 -0700

    add/delete books function now functional

--- src/Header.js
+++ src/Header.js
@@ -6,9 +6,19 @@ class Header extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>Best Books of Every Summer</Navbar.Brand>
-        <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        {/* PLACEHOLDER: render a navigation link to the about page */}
+        <Navbar.Brand>
+          Best Books of Every Summer since the 15th Century
+        </Navbar.Brand>
+        <NavItem>
+          <Link to="/" className="nav-link">
+            The Library
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link to="./About" className="nav-link">
+            About the (site) Author
+          </Link>
+        </NavItem>
       </Navbar>
     )
   }
