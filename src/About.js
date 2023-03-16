0c941126704e8653fc070d7927f5a5bdd1f37ace
Laurel Perkins <123340286+gone-explauren@users.noreply.github.com>
2023-03-15 22:46:34 -0700

    Update About.js

--- src/About.js
+++ src/About.js
@@ -3,16 +3,22 @@ import { Component } from "react";
 class Profile extends Component {
 
   render() {
-    /* Done: render information about the developers */
+
+    /* Done: render information about the developers 
+    Not Done: where are my links..? How do I view this page? What is going wrong? */
+
     return (
+
       <>
-        <h3>Hi, my name is Laurel and I made this :)</h3>
-        <p>I exclusively read poetry and medieval manuscripts, preferablly ones about hunting witches written by 15th centurey Garman clergymen who have wet dreams about ancient Rome while overlooking all the heresy and homosexuality that took place back then.</p>
-        <p>If you try to add a book to this library that does not fit the above criteria, I will send my bookworms out to devour your soul.</p>
+        <h3>Hi, my name is Laurel</h3>
+        <h4>...and I am the curator of this humble Library :)</h4>
+        <p>I exclusively read poetry and medieval manuscripts, preferablly ones about hunting witches written by 15th centurey Garman clergymen who have wet dreams about ancient Rome while overlooking all the heresy and the "synfull" homosexuality that was so commonplace back then.</p>
+        <p>If you try to add a book to this Library that does not fit the above criteria, I will send my bookworms out to devour your soul.</p>
         <p>Like, you won't see them coming, and this website will do nothing to stop you in your devilish quest, but trust me. They will get you. And you will die.</p>
         <br></br>
-        <p>Anyway! Go ahead and relax, enjoy some tea,and indulge in the library's simple pleasures~</p>
+        <p>Anyway! Go ahead and relax, enjoy some warm chamomile and rosehip tea, and indulge in the Library's simple pleasures~</p>
       </>
+
     )
   }
 };
