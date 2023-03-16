d7501040f0e5aab3b55708d90a96b5c0c1ba361b
Laurel Perkins <123340286+gone-explauren@users.noreply.github.com>
2023-03-15 22:48:07 -0700

    Update About.js

--- src/About.js
+++ src/About.js
@@ -12,7 +12,7 @@ class Profile extends Component {
       <>
         <h3>Hi, my name is Laurel</h3>
         <h4>...and I am the curator of this humble Library :)</h4>
-        <p>I exclusively read poetry and medieval manuscripts, preferablly ones about hunting witches written by 15th centurey Garman clergymen who have wet dreams about ancient Rome while overlooking all the heresy and the "synfull" homosexuality that was so commonplace back then.</p>
+        <p>I exclusively read poetry and medieval manuscripts, preferably ones about hunting witches written by 15th centurey Garman clergymen who have wet dreams about ancient Rome while overlooking all the heresy and the "synfull" homosexuality that was so commonplace back then.</p>
         <p>If you try to add a book to this Library that does not fit the above criteria, I will send my bookworms out to devour your soul.</p>
         <p>Like, you won't see them coming, and this website will do nothing to stop you in your devilish quest, but trust me. They will get you. And you will die.</p>
         <br></br>
